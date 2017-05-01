2094cb3a46c848411657764dca982c8b9ee1e23a
kemshiro <i.am.nguyentuanlinh@gmail.com>
2017-05-01 08:39:00 +0700

    Add project files.

---
+++ ShoeWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoeWebsite.Startup))]
+namespace ShoeWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
