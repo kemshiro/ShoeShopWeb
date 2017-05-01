467b6c0a5de0310c16fa4f8b4acc06fcd67791c2
kemshiro <i.am.nguyentuanlinh@gmail.com>
2017-05-01 11:29:34 +0700

    Tạo project mới

---
+++ ShoeWebsite/Areas/AdminArea/Controllers/BasicTableController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoeWebsite.Areas.AdminArea.Controllers
+{
+    public class BasicTableController : Controller
+    {
+        // GET: AdminArea/BasicTable
+        public ActionResult BasicTable()
+        {
+            return View();
+        }
+    }
+}
---
+++ ShoeWebsite/Areas/AdminArea/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoeWebsite.Areas.AdminArea.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: AdminArea/Login
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+    }
+}
